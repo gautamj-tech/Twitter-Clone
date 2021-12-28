06f054f6ecf2d492611341ede6767cc610cd3b1e
Gautam Jain <gautam.jain@letsdressup.in>
2021-12-29 02:46:23 +0530

    Removed unn piece of code

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <router-view :profileData="profileData" />
   </div>
 </template>
@@ -19,33 +15,11 @@ export default {
   async created() {
      const details = await JSON.parse(localStorage.getItem('userDetails'));
     this.profileData = details[0];
-    console.log(this.profileData.name)
-    console.log(this.profileData, "YEHIIIIIIIIII");
   },
 };
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
 * {
   margin: 0;
   padding: 0;
